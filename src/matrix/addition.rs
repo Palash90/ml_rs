22b226120823c9c393033cd2ffa94ef88e77dc2f
Palash Kanti Kundu <palash.kanti.kundu.90@gmail.com>
2024-05-09 01:18:36 +0530

    Addition with Error on Mismatched data length

--- src/matrix/addition.rs
+++ src/matrix/addition.rs
@@ -2,17 +2,21 @@ use super::*;
 use std::ops::Add;
 
 impl<T: Add<Output = T> + Copy> Add for Matrix<T> {
-    type Output = Self;
+    type Output = Result<Matrix<T>, String>;
 
-    fn add(self, rhs: Self) -> Self {
+    fn add(self, rhs: Self) -> Result<Matrix<T>, String> {
         let mut result_vector = Vec::with_capacity(self.data.len());
+
+        if self.data.len() != rhs.data.len() {
+            return Err(String::from("Two matrices data are not of same dimensions"));
+        }
 
         for i in 0..self.data.len() {
             result_vector.push(self.data[i] + rhs.data[i]);
         }
 
-        Self {
+        Ok(Self {
             data: result_vector,
-        }
+        })
     }
 }
--- tests/test_add.rs
+++ tests/test_add.rs
@@ -5,17 +5,34 @@ fn test_add_i32() {
     let m1 = Matrix::new(vec![1, 2]); // Matrix::<i32>::new(vec!(1,2))
     let m2 = Matrix::new(vec![3, 4]);
 
-    let m3 = m1 + m2; // add(m1, m2) ---> m1.add(m2); ---> m1 + m2;
+    let m3: Result<Matrix<i32>, String> = m1 + m2; // add(m1, m2) ---> m1.add(m2); ---> m1 + m2;
+
+    let m3 = m3.unwrap();
 
     assert_eq!(Matrix::new(vec!(4, 6)), m3);
 }
 
 #[test]
 fn test_add_u32() {
-    let m1 = Matrix::new(vec![1u32, 2u32]); // Matrix::<i32>::new(vec!(1,2))
+    let m1 = Matrix::new(vec![1u32, 2u32]);
     let m2 = Matrix::new(vec![3u32, 4u32]);
 
-    let m3 = m1 + m2; // add(m1, m2) ---> m1.add(m2); ---> m1 + m2;
+    let m3 = m1 + m2;
+
+    let m3 = m3.unwrap();
 
     assert_eq!(Matrix::new(vec!(4u32, 6u32)), m3);
+}
+
+#[test]
+#[should_panic]
+fn test_add_panics() {
+    let m1 = Matrix::new(vec![1, 2, 5]);
+    let m2 = Matrix::new(vec![3, 4]);
+
+    let m3 = m1 + m2;
+
+    let m3 = m3.unwrap();
+
+    assert_eq!(Matrix::new(vec!(4, 6)), m3);
 }
