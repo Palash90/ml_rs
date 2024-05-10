78ec954883552057f4038a70d571b1109705de15
Palash Kanti Kundu <palash.kanti.kundu.90@gmail.com>
2024-05-10 16:47:23 +0530

    Refactoring

--- src/tensor/addition.rs
+++ src/tensor/addition.rs
@@ -7,19 +7,19 @@ impl<T: Numeric> Add for Tensor<T> {
     type Output = Result<Tensor<T>, String>;
 
     fn add(self, rhs: Self) -> Result<Tensor<T>, String> {
-        let mut result_vector = Vec::with_capacity(self.data.len());
+        let mut result = Vec::with_capacity(self.data.len());
 
         if self.data.len() != rhs.data.len() {
             return Err(String::from("Two matrices data are not of same dimensions"));
         }
 
         for i in 0..self.data.len() {
-            result_vector.push(self.data[i] + rhs.data[i]);
+            result.push(self.data[i] + rhs.data[i]);
         }
 
         Ok(Self {
             shape: self.shape,
-            data: result_vector,
+            data: result,
         })
     }
 }
